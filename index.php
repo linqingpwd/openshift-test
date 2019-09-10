e67cf4011418087a2dda9ba2f5c528f40c36e500
linq <linq@redhat.com>
2019-09-10 11:05:33 +0800

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body>
 
-<?php echo "Hello World!"; ?>
+<?php echo "exam280"; ?>
 <?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?><br /><br />
 
 <?php $links = [];
